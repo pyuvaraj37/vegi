90f71b3b267c0b341302914bae963315a5d73de1
mikuvey <sgang011@ucr.edu>
2024-05-28 14:39:03 -0700

    gpu support added to easyocr

--- ocr/easy_ocr/easy_inference.py
+++ ocr/easy_ocr/easy_inference.py
@@ -5,6 +5,10 @@ import qlinear
 from utils import Utils
 from timeit import default_timer as timer
 
+# Check if CUDA is available and set the device
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+print(f"Using device: {device}")
+
 #Load the quantized models
 q_detector = torch.load('./models/quantized_detection_model.pt')
 q_recognizer = torch.load('./models/quantized_recognition_model.pt')
@@ -18,6 +22,9 @@ Utils.replace_node(q_recognizer, torch.ao.nn.quantized.dynamic.modules.linear.Li
 
 # Initialize the EasyOCR reader
 reader = easyocr.Reader(['en'])  # Initialize with the desired language
+
+# # Initialize the EasyOCR reader with GPU support if available
+# reader = easyocr.Reader(['en'], gpu=torch.cuda.is_available())
 
 # Replace the models in the reader object
 reader.detector = q_detector
