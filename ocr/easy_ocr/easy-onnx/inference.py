965cfddc3c98f29af5030cb9bfd3e197d7d1b56f
mikuvey <sgang011@ucr.edu>
2024-06-11 19:42:40 -0700

    easy onnx to be added

--- ocr/easy_ocr/easy-onnx/inference.py
+++
@@ -1,8 +0,0 @@
-from EasyOCR import easyocr
-
-reader = easyocr.Reader(['en'])
-
-image_path = "./images/test_image_1.png"
-result = reader.readtext(image_path, detail=0, paragraph=True)
-
-print(result)
